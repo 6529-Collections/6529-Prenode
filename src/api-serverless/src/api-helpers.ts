c292c0acc20eb85232d81662a10cea052519bf07
prxt <prxt@6529.io>
2024-06-12 09:11:21 +0300

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/api-serverless/src/api-helpers.ts
+++ src/api-serverless/src/api-helpers.ts
@@ -5,6 +5,16 @@ import {
   ACCESS_CONTROL_ALLOW_ORIGIN_HEADER,
   corsOptions
 } from './api-constants';
+import { execSync } from 'child_process';
+
+function getCommitHash(): string {
+  try {
+    return execSync('git rev-parse --short HEAD').toString().trim();
+  } catch (error) {
+    console.error('Error retrieving commit hash:', error);
+    return '';
+  }
+}
 
 export function returnJsonResult(result: any, response: Response) {
   response.setHeader(CONTENT_TYPE_HEADER, JSON_HEADER_VALUE);
@@ -12,5 +22,9 @@ export function returnJsonResult(result: any, response: Response) {
     ACCESS_CONTROL_ALLOW_ORIGIN_HEADER,
     corsOptions.allowedHeaders
   );
+  const hash = getCommitHash();
+  if (hash) {
+    result.hash = getCommitHash();
+  }
   response.json(result);
 }
