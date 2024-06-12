e552189214846761b8a369b75120da0bf9a41d2c
prxt <prxt@6529.io>
2024-06-12 09:54:07 +0300

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/api-serverless/src/api-helpers.ts
+++ src/api-serverless/src/api-helpers.ts
@@ -16,15 +16,16 @@ function getCommitHash(): string {
   }
 }
 
+const RELEASE_HASH = getCommitHash();
+
 export function returnJsonResult(result: any, response: Response) {
   response.setHeader(CONTENT_TYPE_HEADER, JSON_HEADER_VALUE);
   response.setHeader(
     ACCESS_CONTROL_ALLOW_ORIGIN_HEADER,
     corsOptions.allowedHeaders
   );
-  const hash = getCommitHash();
-  if (hash) {
-    result.hash = getCommitHash();
+  if (RELEASE_HASH) {
+    result.version = RELEASE_HASH;
   }
   response.json(result);
 }
